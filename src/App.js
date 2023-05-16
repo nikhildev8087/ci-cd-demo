667df1010cb3afd56a2c1c9983445c04a14f7a8f
Nikhil Borge <nikhil.b@prometteur.in>
2023-05-16 11:05:52 +0530

    test 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      ci/ cd test app
+      learn react
      <h1>new app created</h1>
       <h3>New changes added</h3>
     </div>
